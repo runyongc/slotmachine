5cc5d089ed30ffd65dafb17b671ac888a5dced72
Grant Runyon <runyon_gc@newriver.edu>
2018-04-05 13:40:48 -0400

    Added Bets and Classes

--- slotmachine.rb
+++ slotmachine.rb
@@ -1,20 +1,48 @@
-def SlotRoll()
+class SlotMachine
+
+def initialize()
+	@cash = 50
+end
+
+def slotRoll()
 	slots = ["Clubs", "Diamonds", "Spades", "Hearts"]
 	rollone = slots.sample
 	rolltwo = slots.sample
 	rollthree = slots.sample
-	puts "#{RollOne}" + "--" + "#{RollTwo}" + "--" + "RollThree"
-	WinCondition(rollone,rolltwo,rollthree)
+	puts "#{rollone}" + "--" + "#{rolltwo}" + "--" + "#{rollthree}"
+	winCondition(rollone,rollone,rollone)
 end
 
-def WinCondition(slot1, slot2, slot3)
+def winCondition(slot1, slot2, slot3)
 	if slot1 == slot2 && slot2 == slot3
 		puts "Congratulations! You've hit the jackpot!"
+		@cash += @cash + (@bet * 3)
 		return true
 	elsif slot1 == slot2 || slot2 == slot3 || slot1 == slot3
+		@cash += @cash + @bet
 		puts "You've broke even!"
 		return true
 	else 
+		@cash += @cash
+		puts "You've lost!"
 		return false
 	end
 end
+def playGame
+	puts "Welcome to Grant's Casino!"
+	puts "How much would you like to bet? Current Cash:#{@cash}"
+	@bet = gets.chomp.to_i
+	@cash -= @bet
+	puts "Cash remaining after bet: #{@cash}"
+	puts "Alright! Rolling the slots with a bet of #{@bet}!"
+	slotRoll()
+	puts "Would you like to play again? Y/N"
+	if gets.chomp == "Y"
+		playGame
+	end
+end
+end
+newgame = SlotMachine.new
+newgame.playGame
+
+
