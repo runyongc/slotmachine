9df1a0ebc72b054d11a1c82574f2170bfa81713e
Grant Runyon <runyon_gc@newriver.edu>
2018-04-05 14:51:58 -0400

    Added legend, fixed a bug with cash arithmetic.

--- slotmachine.rb
+++ slotmachine.rb
@@ -10,34 +10,34 @@ def slotRoll()
 	rolltwo = slots.sample
 	rollthree = slots.sample
 	puts "#{rollone}" + "--" + "#{rolltwo}" + "--" + "#{rollthree}"
-	winCondition(rollone,rollone,rollone)
+	winCondition(rollone,rolltwo,rollthree)
 end
 
 def winCondition(slot1, slot2, slot3)
 	if slot1 == slot2 && slot2 == slot3
-		puts "Congratulations! You've hit the jackpot!"
+		puts "Congratulations! You've hit the jackpot! Winnings: #{(@bet * 3)}"
 		@cash += @cash + (@bet * 3)
 		return true
 	elsif slot1 == slot2 || slot2 == slot3 || slot1 == slot3
-		@cash += @cash + @bet
+		@cash += @bet
 		puts "You've broke even!"
 		return true
 	else 
-		@cash += @cash
 		puts "You've lost!"
 		return false
 	end
 end
 def playGame
 	puts "Welcome to Grant's Casino!"
-	puts "How much would you like to bet? Current Cash:#{@cash}"
+	puts "Last Bet: #{@bet} Current Cash: #{@cash}"
+	puts "How much would you like to bet?"
 	@bet = gets.chomp.to_i
 	@cash -= @bet
-	puts "Cash remaining after bet: #{@cash}"
 	puts "Alright! Rolling the slots with a bet of #{@bet}!"
 	slotRoll()
 	puts "Would you like to play again? Y/N"
-	if gets.chomp == "Y"
+	playagain = gets.chomp.downcase
+	if playagain == "y"
 		playGame
 	end
 end
